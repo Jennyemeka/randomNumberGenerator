0711abb6f980d78e490593e44eb3fb6b79493aa3
jenni_emeka <jenniferemeka24@gmail.com>
2024-01-20 04:45:13 +0100

    created gameEngine method

--- src/randomNumberGenerator/GenerateNumber.java
+++ src/randomNumberGenerator/GenerateNumber.java
@@ -4,44 +4,43 @@ import java.util.Scanner;
 import java.util.Random;
 import javax.swing.JOptionPane;
 public class GenerateNumber {
+	
+	public static void gameEngine() {
+		Scanner scanner = new Scanner(System.in);
+
+		Random random = new Random();
+		int randomNum = random.nextInt(10)+ 1;
+		boolean correctGuess = false;
+		for (int i = 1; i <= 5; i++) {
+			int userInput = scanner.nextInt();
+			if(userInput == randomNum) {
+				System.out.println("You got it");
+				correctGuess = true;
+				break;
+			}else if(userInput < randomNum) {
+				System.out.println("Guessed number too low!");
+			}else {
+				System.out.println("Guessed number too high!");
+			}
+			
+		}
+		if(!correctGuess) {
+			System.out.println("You have exceeded your guesses!");
+		}
+	}
 
 	public static void main(String[] args) {
-		boolean correctGuess = false;
+
 		try {
+			System.out.println("Enter a random number. You have 10 guesses");
+			gameEngine();
 
-		    Scanner scanner = new Scanner(System.in);
-
-		     Random random = new Random();
-		     int randomNum = random.nextInt(1)+1;
-
-			System.out.println("Enter a random number. You can only guess 5 times");
-		     
-		     for(int i = 1;i <= 5; i++) {
-		    	 int userInput = scanner.nextInt();
-					
-				     
-				  if(userInput == randomNum) {	
-		    		   System.out.println("You got it!");
-		    		   correctGuess = true;
-		    		   break;
-		    	     }
-		    	   else if (userInput < randomNum) {
-		    	    	 System.out.println("Guessed number too low!");
-		    	     }
-		    	   else {
-		    		   System.out.println("Guessed number too high!");
-		    	   }
-		    	  
-		    	 }
-		 	    
-		 	    if(!correctGuess) {
-				System.out.println("You have exceeded your guesses!");
-	}
-			
 		} catch(java.util.InputMismatchException e)  {
 			
 			System.out.println("You entered an invalid number");
-				
+			System.out.println("Try one more time, Enter a random number");
+			gameEngine();
+			
 			
 		} 
   
