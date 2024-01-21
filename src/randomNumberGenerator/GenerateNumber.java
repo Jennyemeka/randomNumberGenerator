a386ed3218a067b0cdd608d90141fffe240e0da4
jenni_emeka <jenniferemeka24@gmail.com>
2024-01-21 19:02:28 +0100

    update

--- src/randomNumberGenerator/GenerateNumber.java
+++ src/randomNumberGenerator/GenerateNumber.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 import java.util.Random;
 import javax.swing.JOptionPane;
 public class GenerateNumber {
-	
 	public static void gameEngine() {
 		Scanner scanner = new Scanner(System.in);
 
@@ -22,7 +21,6 @@ public class GenerateNumber {
 			}else {
 				System.out.println("Guessed number too high!");
 			}
-			
 		}
 		if(!correctGuess) {
 			System.out.println("You have exceeded your guesses!");
@@ -30,13 +28,10 @@ public class GenerateNumber {
 	}
 
 	public static void main(String[] args) {
-
 		try {
 			System.out.println("Enter a random number. You have 10 guesses");
 			gameEngine();
-
 		} catch(java.util.InputMismatchException e)  {
-			
 			System.out.println("You entered an invalid number");
 			System.out.println("Try one more time, Enter a random number");
 			gameEngine();
